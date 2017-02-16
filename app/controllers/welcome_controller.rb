9aad1fbed5ee640cdfb9b22dd4caeed378822f6e
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2017-02-16 00:07:38 -0300

    Se modifico la fuente de texto y se avanzo con la pagina inicial parte 2, 3 y 4

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
-  def index
+	def index
+		nacimiento = Time.local(1993, 9, 14, 0, 0, 0)
+		ahora = Time.now
+		if ahora.month > nacimiento.month || (nacimiento.month == nacimiento.month && ahora.day >= nacimiento.day)
+  			@cantidad_de_anios = ahora.year - nacimiento.year
+  		else
+  			@cantidad_de_anios = ahora.year - nacimiento.year - 1
+  		end
   end
 end
