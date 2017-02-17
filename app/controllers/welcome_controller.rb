82d260aec169f6c2363d03a99959504cb8096a1e
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2017-02-17 01:02:35 -0300

    Se arreglo los contactos sin la funcionalidad, y la pag principal con las alineaciones

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,10 @@ class WelcomeController < ApplicationController
 	def index
 		nacimiento = Time.local(1993, 9, 14, 0, 0, 0)
 		ahora = Time.now
-		if ahora.month > nacimiento.month || (nacimiento.month == nacimiento.month && ahora.day >= nacimiento.day)
-  			@cantidad_de_anios = ahora.year - nacimiento.year
-  		else
-  			@cantidad_de_anios = ahora.year - nacimiento.year - 1
-  		end
-  end
+		if ahora.month > nacimiento.month || (ahora.month == nacimiento.month && ahora.day >= nacimiento.day)
+			@cantidad_de_anios = ahora.year - nacimiento.year
+		else
+			@cantidad_de_anios = ahora.year - nacimiento.year - 1
+		end
+	end
 end
