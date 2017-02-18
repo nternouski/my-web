aa91bac06a7fea3f52befd659ab837c774e1f855
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2017-02-18 19:13:12 -0300

    Deshabilitando errores mail y provando mensaje de satisfaccion

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,8 +4,9 @@ class ContactController < ApplicationController
 			if params[:email].empty? || params[:nombre].empty? || params[:mensaje].empty?
 				flash[:danger]="Los campos nombre, mail y mensaje no deben estar vacios."
 			else
-				UserMailer.contact(params[:nombre],params[:email],params[:asunto],params[:mensaje]).deliver
-				flash[:success]="Su mail se envio correctamente."
+				if UserMailer.contact(params[:nombre],params[:email],params[:asunto],params[:mensaje]).deliver
+					flash[:success]="Su mail se envio correctamente."
+				end
 			end
 		end
 	end
