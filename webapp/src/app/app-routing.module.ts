db946ee2873abed5870cabb9b436ba914de1f21f
nternouski <nahuelternouski@gmail.com>
2021-07-07 22:10:46 -0300

    Nueva version de front a 12

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+	imports: [
+		RouterModule.forRoot(routes, {
+			preloadingStrategy: PreloadAllModules,
+			relativeLinkResolution: "legacy"
+		})
+	],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
