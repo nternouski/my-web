785f4202c3224e52a85f5c39b7c64d7a328c838c
nternouski <nahuelternouski@gmail.com>
2021-07-07 21:36:46 -0300

    Actualizacion de proyectos

--- webapp/src/app/views/welcome/projects/projects.component.ts
+++ webapp/src/app/views/welcome/projects/projects.component.ts
@@ -10,11 +10,6 @@ export class ProjectsComponent {
 	projects = [
 		{ link: "https://github.com/nternouski/my-web", title: "my-web", img: "pag-web.png" },
 		{
-			link: "https://nosrobaronelnombre.herokuapp.com/",
-			title: "gauchada",
-			img: "gauchada.png"
-		},
-		{
 			link: "https://www.youtube.com/watch?v=cNLXL_kVEpY",
 			title: "design3d",
 			img: "agua-3d.png"
@@ -25,6 +20,27 @@ export class ProjectsComponent {
 			link: "https://www.youtube.com/watch?v=KX1nlDv9_-0",
 			title: "espirales",
 			img: "espirales.png"
+		},
+		{
+			link:
+				"https://drive.google.com/file/d/1GWYDNdnA0nP2F8v1D8bUU4smXH559QaO/view?usp=sharing",
+			title: "acordar-branding",
+			img: "branding-acordar.jpg"
+		},
+		{
+			link: "https://youtu.be/oVZ2zidVboc",
+			title: "acordar-draft",
+			img: "draft-acordar.jpg"
+		},
+		{
+			link: "https://www.youtube.com/playlist?list=PLcj82OLSpnaZVzmATfwBmxkpypBk_4o9I",
+			title: "mauna-story",
+			img: "mauna-story.jpg"
+		},
+		{
+			link: "https://www.youtube.com/watch?v=2CF4WiVOGNs&t=2s",
+			title: "elremanso",
+			img: "elremanzo-mauna.jpg"
 		}
 	];
 
--- webapp/src/app/views/welcome/quality/quality.component.ts
+++ webapp/src/app/views/welcome/quality/quality.component.ts
@@ -9,15 +9,18 @@ export class QualityComponent {
 	multimedia = {
 		name: "Multimedia",
 		elements: [
-			{ name: "Adobe Illustrator", stars: ["star", "star", "star", "star", "star_border"] },
 			{ name: "Adobe After Effect", stars: ["star", "star", "star", "star", "star_border"] },
-			{ name: "Adobe Photoshop", stars: ["star", "star", "star", "star", "star_border"] },
+			{ name: "Adobe Illustrator", stars: ["star", "star", "star", "star", "star_border"] },
 			{
-				name: "Adobe Audition",
+				name: "Adobe Photoshop",
 				stars: ["star", "star", "star", "star_border", "star_border"]
 			},
 			{
-				name: "Adobe Premiere",
+				name: "Adobe InDesign",
+				stars: ["star", "star", "star", "star_border", "star_border"]
+			},
+			{
+				name: "Adobe Audition",
 				stars: ["star", "star", "star_border", "star_border", "star_border"]
 			},
 			{
@@ -30,16 +33,14 @@ export class QualityComponent {
 	languagePrograming = {
 		name: "welcome.knowledge.language_programing",
 		elements: [
-			{ name: "Latex", stars: ["star", "star", "star", "star", "star"] },
-			{ name: "Angular", stars: ["star", "star", "star", "star", "star_border"] },
+			{ name: "Node", stars: ["star", "star", "star", "star", "star"] },
+			{ name: "Angular", stars: ["star", "star", "star", "star", "star"] },
+			{ name: "Git", stars: ["star", "star", "star", "star", "star"] },
+			{ name: "GCP", stars: ["star", "star", "star", "star", "star_border"] },
 			{ name: "CSS/HTML/JS", stars: ["star", "star", "star", "star", "star_border"] },
+			{ name: "Python", stars: ["star", "star", "star", "star_border", "star_border"] },
 			{ name: "Bash ", stars: ["star", "star", "star", "star_border", "star_border"] },
-			{ name: "C/C++", stars: ["star", "star", "star_border", "star_border", "star_border"] },
-			{
-				name: "Ruby on Rails",
-				stars: ["star", "star", "star_border", "star_border", "star_border"]
-			},
-			{ name: "MATLAB", stars: ["star", "star", "star_border", "star_border", "star_border"] }
+			{ name: "C/C++", stars: ["star", "star", "star_border", "star_border", "star_border"] }
 		]
 	};
 
