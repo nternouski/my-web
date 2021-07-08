e67a763cf301962e6863e6d2cc284998b7e04ce0
nternouski <nahuelternouski@gmail.com>
2021-07-07 21:54:35 -0300

    Actualizacion de backend entre otros

--- functions/src/http/index.ts
+++ functions/src/http/index.ts
@@ -1,12 +1,6 @@
 import * as express from "express";
-import * as multer from "multer";
 
 const app = express();
 app.disable("x-powered-by");
-// handle multipart form data
-app.use([multer().any()]);
-
-// import { listener as ocr } from "./text-detection";
-// app.use("/api/ocr", ocr);
 
 export const listener = app;
